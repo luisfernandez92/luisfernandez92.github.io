0f0bb0809da81cdafaded1aea424f2acf3a85e4b
Luis Fernandez <luisfdez1992@gmail.com>
2018-04-13 17:43:43 -0500

    Second Commit

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ links.forEach(function(link){
     content.classList.add("animated");
 
     setTimeout(function(){
-      location.href = "/";
+      location.href = "/sdsd";
     }, 600);
 
   });
